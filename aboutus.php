6bb8d6e409d59ea1319ed64a68768ab0bef48224
sandra2022 <mca544@rajagiri.edu>
2021-03-17 11:21:01 +0530

    homework

---
+++ aboutus.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        img {
+          width: 90%;
+          height: auto;
+        }
+        </style>
+        </head>
+        <body>
+        <img src="abt3.jfif" width="50" height="50">
+        <p><b><i><h3>we..</h3></i></b></p>
+        <img src="km.jfif" width="50" height="50">
+        <p><i><h2><b>His fans range from the age group of 6-80, a feat surpassed only by Bollywood personalities. Men, women, children,soon-to-be-married girls, housewives, professionals, mothers, mothers-in-law, grandmothers! For a nation that loves to eat and cook, Sanjeev Kapoor is very close to its people’s hearts. He is the go-to chef for everything…party at home? What-to-make-today situation? So much paneer, what to do? One stop solution – log on to the internet or his channel and cook up SanjeevKapoor’s recipes!
+        </b></h2></i></p>
+        <img src="abt.jfif" width="30" height="30">
+        <p><i><h3><b>Not surprising then that he figured in the Reader’s Digest list of 100 of India’s Most Trusted people. And why not? Ask those billions of Indians out there and their answer will be unanimous, when it comes to the most known and trusted faces on TV, Chef extraordinaire Sanjeev Kapoor’s name tops the list! His easy demeanour, cheerful commentary and simple instructions make the most seemingly difficult dishes look easy to prepare. He has been awarded the prestigious Padma Shri Award 2017. His popularity and contribution to Indian cuisine has been recognized by the Government of India too, who conferred on him the National award of “Best Chef of India.” He is also on the panel of the Ministry of Tourism. He is on the board of Singapore Airlines International Culinary Panel along with the brightest names from the culinary world across the global culinary capitals. He divides his time in India and abroad by doing live food events and cooking demonstrations for which is he is always in great demand.</b></h3></i></p>
+        <img src="abt2.jfif" width="30" height="30">
+        <p><i><h3><b>n addition to his achievements on his homeground, he has won accolades and honours abroad too, not to mention the many firsts he has in his kitty. The first chef in the world to launch his own food channel FoodFood. His earlier food show KhanaKhazana ran for 18 years on a trot on ZEE channel, a record that is yet to be broken. Needless to say, the show also won the Best Cookery Show award from the Indian Television Academy (ITA) and Indian Telly Awards, year after year. He was on the panel of Judges for Master Chef India Season 3 and 4. He served as the Chief Judge and host for ‘Sanjeev Kapoor ke Kitchen Khiladi’ on Sony Entertainment Television. His life story featured on Star World as part of the exclusive series titled Achiever’s Club.He is also ranked 73rd in the Forbes list of top 100 Indian celebrities.He has also cooked for many dignitaries, including the Honourable Prime Minister Shri Narendra Modi.</b></h3></i></p>
+    </body>
+</html>
